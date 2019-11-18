ad7920ec43151fbe1e82967ab0a554627f518cb0
Caio Lopes <caiotuxo@gmail.com>
2019-11-18 17:50:22 -0300

    alias bank

--- lib/cdt_baas/modules/cdt_modules.rb
+++ lib/cdt_baas/modules/cdt_modules.rb
@@ -1,6 +1,6 @@
 require_relative 'cdt_auth'
-require_relative 'cdt_alias'
 require_relative 'cdt_module'
+require_relative 'cdt_alias'
 require_relative 'cdt_person'
 require_relative 'cdt_address'
 require_relative 'cdt_phone'
