9d9ec14cc334ca4a7f25fba2bad76e0768f4da84
Caio Lopes <caiotuxo@gmail.com>
2020-01-10 17:34:28 -0300

    individuals version as param

--- lib/cdt_baas/modules/cdt_individual.rb
+++ lib/cdt_baas/modules/cdt_individual.rb
@@ -7,32 +7,32 @@ module CdtBaas
             startModule(token, env)
         end
 
-        def createAccount(body)
-            response = @request.postWithHeader(@url + "v2/" + INDIVIDUALS_ACCOUNTS, body, [{:key => 'Content-Type', :value => "application/json"}])
+        def createAccount(body, version = 2)
+            response = @request.postWithHeader(@url + "v#{version}/" + INDIVIDUALS_ACCOUNTS, body, [{:key => 'Content-Type', :value => "application/json"}])
             person = CdtModel.new(response)
             person
         end
 
-        def createPerson(body)
-            response = @request.postWithHeader(@url + "v2/" + INDIVIDUALS, body, [{:key => 'Content-Type', :value => "application/json"}])
+        def createPerson(body, version = 2)
+            response = @request.postWithHeader(@url + "v#{version}/" + INDIVIDUALS, body, [{:key => 'Content-Type', :value => "application/json"}])
             person = CdtModel.new(response)
             person
         end
 
-        def findPerson(id)
-            response = @request.get(@url + "v2/" + INDIVIDUALS + id.to_s)
+        def findPerson(id, version = 2)
+            response = @request.get(@url + "v#{version}/" + INDIVIDUALS + id.to_s)
             person = CdtModel.new(response)
             person
         end
 
-        def getPersons(body)
-            response = @request.get(@url + "v2/" + INDIVIDUALS + CdtHelper.conductorBodyToString(body))
+        def getPersons(body, version = 2)
+            response = @request.get(@url + "v#{version}/" + INDIVIDUALS + CdtHelper.conductorBodyToString(body))
             person = CdtModel.new(response)
             person
         end
 
-        def updatePerson(id, body)
-            response = @request.put(@url + "v2/" + INDIVIDUALS + id.to_s , body, [{:key => 'Content-Type', :value => "application/json"}])
+        def updatePerson(id, body, version = 2)
+            response = @request.put(@url + "v#{version}/" + INDIVIDUALS + id.to_s , body, [{:key => 'Content-Type', :value => "application/json"}])
             person = CdtModel.new(response)
             person
         end
