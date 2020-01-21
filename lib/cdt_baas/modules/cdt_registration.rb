46d61ea5b1d6cd8af387ac8761b0561ca28bacbd
Caio Lopes <caiotuxo@gmail.com>
2020-01-21 11:12:23 -0300

    update registrations

--- lib/cdt_baas/modules/cdt_registration.rb
+++ lib/cdt_baas/modules/cdt_registration.rb
@@ -22,14 +22,14 @@ module CdtBaas
         end
         
         def findCompany(id, version = 1)
-            @url = @url.gsub("api", "companies") + 'v' + version.to_s + '/'
+            @url = @url.gsub("api", "companies") # + 'v' + version.to_s + '/'
             response = @request.get(@url + COMPANY_REGISTRATIONS + '/' + id.to_s)
             person = CdtModel.new(response)
             person
         end
 
         def updateRegistration(id, body, version = 1)
-            @url = @url.gsub("api", "companies") + 'v' + version.to_s + '/'
+            @url = @url.gsub("api", "companies") # + 'v' + version.to_s + '/'
             response = @request.put(@url + COMPANY_REGISTRATIONS + '/' + id.to_s)
             person = CdtModel.new(response)
             person
