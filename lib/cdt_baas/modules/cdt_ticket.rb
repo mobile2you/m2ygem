ba46537a118f6ae7367339223bde891d1d828560
Gustavo Fazani da Silva <gustavo.fazani@mobile2you.com.br>
2020-04-23 19:51:50 -0300

    Payment slip v1

---
+++ lib/cdt_baas/modules/cdt_ticket.rb
@@ -0,0 +1,33 @@
+module CdtBaas
+  class CdtPaymentSplip < CdtModule
+    def initialize(token, env)
+      startModule(token, env)
+    end
+
+    def create_recharge(body, version = 1)
+      @url = @url.gsub('api.', 'paymentslip.')
+      path = "v#{version}/#{PAYMENT_SLIP_RECHARGE}"
+      @request.postWithHeader(@url + path, body, [{ key: 'Content-Type', value: 'application/json' }])
+    end
+
+    def recharges(body, version = 1)
+      @url = @url.gsub('api.', 'paymentslip.')
+      path = "v#{version}/#{PAYMENT_SLIP_RECHARGE}"
+      @request.get(@url + path + CdtHelper.conductorBodyToString(body))
+    end
+
+    def create_invoice(params, version = 1)
+      @url = @url.gsub('api.', 'paymentslip.')
+      path = "v#{version}/#{PAYMENT_SLIP_INVOICE}" + CdtHelper.conductorBodyToString(params[:query_params])
+      params = params[:body_params]
+      @request.postWithHeader(@url + path, params, [{ key: 'Content-Type', value: 'application/json' }])
+    end
+
+    def invoices(body, version = 1)
+      @url = @url.gsub('api.', 'paymentslip.')
+      path = "v#{version}"
+      @request.get(@url + path + CdtHelper.conductorBodyToString(body))
+    end
+  end
+end
+
