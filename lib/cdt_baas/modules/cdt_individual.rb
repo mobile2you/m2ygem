85e6be0612d783c87436f97e49ed2577150b7903
Paulo Prescendo <paulovinicius1994@hotmail.com>
2020-05-06 17:42:10 -0300

    added individuals getDocuments

--- lib/cdt_baas/modules/cdt_individual.rb
+++ lib/cdt_baas/modules/cdt_individual.rb
@@ -43,6 +43,12 @@ module CdtBaas
             person
         end
 
+        def getDocuments(registration_id, version = 2)
+            response = @request.get(@url + "v#{version}/" + INDIVIDUALS + registration_id.to_s + "/" + DOCUMENTS)
+            person = CdtModel.new(response)
+            person
+        end
+
         def sendDocument(registration_id, params, options, version = 2)
             options[:headers] = {
                 'Content-Type' => 'image/jpeg',
