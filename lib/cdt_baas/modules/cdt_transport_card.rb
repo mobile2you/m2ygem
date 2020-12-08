8fd009a72be53b5196572dee0d3caf580d12e5e4
Caio Lopes <caiotuxo@gmail.com>
2020-12-08 09:12:25 -0300

    ted in

--- lib/cdt_baas/modules/cdt_transport_card.rb
+++ lib/cdt_baas/modules/cdt_transport_card.rb
@@ -1,33 +1,32 @@
 module CdtBaas
 
-	class CdtTransportCard < CdtModule
+  class CdtTransportCard < CdtModule
 
-		def initialize(token, env)
-			startModule(token, env)
-		end
+    def initialize(token, env)
+      startModule(token, env)
+    end
 
-		def transportCards(number)
-			response = @request.get(@url + TRANSPORT_CARDS + number.to_s)
-			person = CdtModel.new(response)
-			person
-		end
+    def transportCards(number)
+      response = @request.get(@url + TRANSPORT_CARDS + number.to_s)
+      person = CdtModel.new(response)
+      person
+    end
 
-		def getTransportCards(body)
-			response = @request.get(@url + TRANSPORT_CARDS_RECHARGE + CdtHelper.conductorBodyToString(body), {}, false)
-			person = CdtModel.new(response)
-			person
-		end
+    def getTransportCards(body)
+      response = @request.get(@url + TRANSPORT_CARDS_RECHARGE + CdtHelper.conductorBodyToString(body), {}, false)
+      person = CdtModel.new(response)
+      person
+    end
 
-		def newRecharge(body)
-			response = @request.post(@url + TRANSPORT_CARDS_RECHARGE, body, true)
-			person = CdtModel.new(response)
-			person
-		end
+    def newRecharge(body)
+      response = @request.post(@url + TRANSPORT_CARDS_RECHARGE, body, true)
+      person = CdtModel.new(response)
+      person
+    end
 
-		def getRecharges(number)
-			response = @request.get(@url + TRANSPORT_CARDS_ACCOUNT + number.to_s, [jsonHeader], false)
-			response
-		end
-
-	end
+    def getRecharges(number)
+      response = @request.get(@url + TRANSPORT_CARDS_ACCOUNT + number.to_s, [jsonHeader], false)
+      response
+    end
+  end
 end
