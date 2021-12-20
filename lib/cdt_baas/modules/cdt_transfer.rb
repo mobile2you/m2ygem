f256264370e40567e991c5222b1f31ec866d0b7c
Caio Lopes <caio.lopes@mobile2you.com.br>
2021-12-20 11:39:28 -0300

    ted body

--- lib/cdt_baas/modules/cdt_transfer.rb
+++ lib/cdt_baas/modules/cdt_transfer.rb
@@ -7,24 +7,23 @@ module CdtBaas
     end
 
     def bankTransfers(original_body)
-      body = {
-        {
-          "idOriginAccount": original_body[:idOriginAccount],
-          "identificator": original_body[:identificator],
-          "description": original_body[:description],
-          "beneficiary": {
-            "type": original_body[:beneficiary][:type],
-            "docIdCpfCnpjEinSSN": original_body[:beneficiary][:docIdCpfCnpjEinSSN],
-            "name": original_body[:beneficiary][:name],
-            "bankId": original_body[:beneficiary][:bankId],
-            "agency": original_body[:beneficiary][:agency],
-            "agencyDigit": "",
-            "account": original_body[:beneficiary][:account],
-            "accountDigit": original_body[:beneficiary][:accountDigit],
-            "accountType": original_body[:beneficiary][:accountType]
-          },
-          "value": original_body[:value]
-        }
+      body =
+      {
+        "idOriginAccount": original_body[:idOriginAccount],
+        "identificator": original_body[:identificator],
+        "description": original_body[:description],
+        "beneficiary": {
+          "type": original_body[:beneficiary][:type],
+          "docIdCpfCnpjEinSSN": original_body[:beneficiary][:docIdCpfCnpjEinSSN],
+          "name": original_body[:beneficiary][:name],
+          "bankId": original_body[:beneficiary][:bankId],
+          "agency": original_body[:beneficiary][:agency],
+          "agencyDigit": "",
+          "account": original_body[:beneficiary][:account],
+          "accountDigit": original_body[:beneficiary][:accountDigit],
+          "accountType": original_body[:beneficiary][:accountType]
+        },
+        "value": original_body[:value]
       }
       response = @request.post(@url + BANK_TRANSFER, body, true)
       transferResponse = CdtModel.new(response)
