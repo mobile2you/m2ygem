bc41f644efef0da0ae5f2694b4cb9cc550e1fe06
Tomas Caldeira <tomas.caldeira@mobile2you.com.br>
2022-07-11 12:25:55 -0300

    using label instead of default value

--- lib/cdt_baas/modules/cdt_individual.rb
+++ lib/cdt_baas/modules/cdt_individual.rb
@@ -25,7 +25,7 @@ module CdtBaas
             person
         end
 
-        def findPerson(id, version = 2, params = {})
+        def findPerson(id, version = 2, params: {})
             response = @request.get(@url + "v#{version}/" + INDIVIDUALS + '/' + id.to_s + CdtHelper.conductorBodyToString(params))
             person = CdtModel.new(response)
             person
