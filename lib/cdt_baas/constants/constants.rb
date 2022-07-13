bbe3e0bc4ea68c1f1bb0b2a46486ad0aeef51c9f
Guilherme Monteiro <guilherme.monteiro@mobile2you.com.br>
2022-07-13 13:55:36 -0300

    feat(cashoutsDock):(Fixing url)

--- lib/cdt_baas/constants/constants.rb
+++ lib/cdt_baas/constants/constants.rb
@@ -1,121 +1,121 @@
 module CdtBaas
+  # envs
+  HOMOLOGATION = 'hml'
+  PRODUCTION = 'prd'
+  CUSTOM_AUTH = 'prdbr'
 
-  #envs
-  HOMOLOGATION = "hml"
-  PRODUCTION = "prd"
-  CUSTOM_AUTH = "prdbr"
+  # urls
+  URL_HML = 'https://api.hml.caradhras.io/'
+  URL_PRD = 'https://api.caradhras.io/'
+  URL_CUSTOM_AUTH = 'https://api.caradhras.io/'
+  URL_CUSTOM_TRANSFER_HML = 'https://transfers.hml.caradhras.io/'
+  URL_CUSTOM_TRANSFER = 'https://transfers.caradhras.io/'
 
-  #urls
-  URL_HML = "https://api.hml.caradhras.io/"
-  URL_PRD = "https://api.caradhras.io/"
-  URL_CUSTOM_AUTH = "https://api.caradhras.io/"
+  # auth_url
+  TOKEN_HML = 'https://auth.hml.caradhras.io/'
+  TOKEN_PRD = 'https://auth.caradhras.io/'
 
-  #auth_url
-  TOKEN_HML = "https://auth.hml.caradhras.io/"
-  TOKEN_PRD = "https://auth.caradhras.io/"
+  # custom_auth
+  TOKEN_CUSTOM_AUTH = ENV['CUSTOM_AUTH_URL']
+  CUSTOM_AUTH_USER = ENV['CUSTOM_AUTH_USER']
+  CUSTOM_AUTH_PASSWORD = ENV['CUSTOM_AUTH_PASS']
 
-  #custom_auth
-  TOKEN_CUSTOM_AUTH = ENV["CUSTOM_AUTH_URL"]
-  CUSTOM_AUTH_USER = ENV["CUSTOM_AUTH_USER"]
-  CUSTOM_AUTH_PASSWORD = ENV["CUSTOM_AUTH_PASS"]
-
-
-  API = "api/"
-  TOKEN_PATH = "oauth2/token?grant_type=client_credentials"
-  DETAILS = "details"
-  RATE = "rate/"
-  ACCOUNT = "account/"
-  PEOPLE_PATH = "pessoas"
-  PEOPLE_DETAILS_PATH = "pessoas-detalhes"
-  ZIP_PATH = "ceps"
-  ADDRESSES_PATH = "enderecos"
-  PHONES_PATH = "telefones"
-  INVOICES_PATH = "boletos"
-  INVOICE_REGISTER_PATH = "registrar"
-  INVOICE_PRINT_PATH = "arquivo.pdf"
-  PDF = "pdf"
-  NEW_ACCOUNT_PATH = "accounts/"
-  ACCOUNT_PATH = "contas"
-  TIMELINE_PATH = "timeline"
-  TRANSACTIONS_PATH = "transacoes"
-  NEW_TRANSACTIONS_PATH = "transactions"
-  ACTIVE_PATH = "reativar"
-  BLOCKED_PATH = "bloquear"
-  CANCEL_PATH = "cancelar"
-  ADDITIONAL = "/additional"
-  CARD_PATH = "gerar-cartao-grafica"
-  VIRTUAL_CARD_PATH = "gerar-cartao-virtual"
-  BANK_SLIP_PATH = "gerar-boleto-recarga"
-  P2P_PATH = "p2ptransfer"
-  V1_PAYMENT = "v1/payments"
-  PAYMENT = "payments/"
-  PAYMENT_VALIDATE = "payments/validate/"
-  VALIDATE = "validate/"
-  P2P_ADJUSTMENT = "p2ptransfer/adjustment/"
-  PAYMENT_ADJUSTMENT = "payments/adjustment/"
-  BANK_SLIP = "boletos"
-  CARD_PLAIN = "cartoes"
-  CARD = "cartoes/"
-  CARD_CONSULT = "consultar-dados-reais"
-  PASSWORD_VALIDATION = "validar-senha"
-  LIMIT_CONSULT = "limites-disponibilidades"
-  CREATE_PF_CARD = "gerar-cartao-pf"
-  CREATE_PJ_CARD = "gerar-cartao-pj"
-  CREATE_NEW_CARD = "gerar-nova-via"
-  UNLOCKED_INCORRECT_PASSWORD = "desbloquear-senha-incorreta"
-  CREATE_DEBIT_CARD = "gerar-Cartao-pre-pago"
-  REGISTER_PASSWORD = "cadastrar-senha"
-  UNLOCKED = "desbloquear"
-  ADD_HOLDER = "atribuir-titular"
-  UPDATE_PASSWORD = "alterar-senha"
-  REGISTER_PF_ACCOUNT = "clientes/pf"
-  REGISTER_PJ_ACCOUNT = "clientes/pj"
-  GENERATE_DOCUMENT = "generatedocument"
-  BANK_TRANSFER = "banktransfers"
-  BANK_TRANSFER_ADJUST = "banktransfers/adjustment/"
-  TRANSPORT_CARDS = "transportcards/card/"
-  TRANSPORT_CARDS_RECHARGE = "transportcards/recharge"
-  TRANSPORT_CARDS_ACCOUNT = "transportcards/account/"
-  RECHARGES = "recharges"
-  DEALERS = "dealers"
-  CONFIRM = "confirm"
-  CONSULT = "consult/"
-  ADJUSTMENT = "adjustment/"
-  REGISTRATION = "registration/"
-  REGISTRATION_WITH_TYPES = "?types=PRIVACY_POLICY&types=TERMS_OF_USE"
-  AGREEMENT = "agreement"
-  USER = "user"
-  LOGIN = "login"
-  ASSIGN_PROFILE = "assign-profile/"
-  UNASSIGN_PROFILE = "unassign-profile/"
-  PRE_PAID = "lotes-cartoes-pre-pagos"
-  BILLET = "billet"
-  CARRIER_DETAILS = "carrier-details"
-  CARRIER = "carrier"
-  CALCULATE = "calculate"
-  CEPS = "ceps"
-  WAIVED = "rate/waived"
-  KYC = "knowyourclient/people"
-  PRODUCTS_PATH = "produtos"
-  INDIVIDUALS = "individuals"
-  REGISTRATIONS = "registrations"
-  COMPANY_REGISTRATIONS = "registered/companies"
-  COMPANY_NATIONAL_REGISTRATIONS = "registrations/nationalRegistration"
-  INDIVIDUALS_ACCOUNTS = "individuals/accounts"
-  SMS_PATH = "omnichannel/sms"
-  RECEIPTS = "receipts"
-  SCHEDULER = "scheduler"
-  ALIAS_BANK = "v1/accounts"
-  PAYMENT_SLIP_RECHARGE = "recharge"
-  PAYMENT_SLIP_INVOICE = "invoice"
-  ADD_NONAME_TO_ACCOUNT = "atribuir-cartao-prepago"
-  DOCSPY_DOCUMENTS = "docspy/documents"
-  DOCSPY_SELFIES = "docspy/selfies"
-  DOCSPY_DOCUMENTS_RULES = "docspy/documents/rules"
-  DOCSPY_SELFIES_RULES = "docspy/selfies/rules"
-  DOCUMENTS = "documents"
-  PENDING_DOCUMENTS = "documents/status"
-  CONTROLS_LIMITS = "/controles-limites"
+  API = 'api/'
+  TOKEN_PATH = 'oauth2/token?grant_type=client_credentials'
+  DETAILS = 'details'
+  RATE = 'rate/'
+  ACCOUNT = 'account/'
+  PEOPLE_PATH = 'pessoas'
+  PEOPLE_DETAILS_PATH = 'pessoas-detalhes'
+  ZIP_PATH = 'ceps'
+  ADDRESSES_PATH = 'enderecos'
+  PHONES_PATH = 'telefones'
+  INVOICES_PATH = 'boletos'
+  INVOICE_REGISTER_PATH = 'registrar'
+  INVOICE_PRINT_PATH = 'arquivo.pdf'
+  PDF = 'pdf'
+  NEW_ACCOUNT_PATH = 'accounts/'
+  ACCOUNT_PATH = 'contas'
+  TIMELINE_PATH = 'timeline'
+  TRANSACTIONS_PATH = 'transacoes'
+  NEW_TRANSACTIONS_PATH = 'transactions'
+  ACTIVE_PATH = 'reativar'
+  BLOCKED_PATH = 'bloquear'
+  CANCEL_PATH = 'cancelar'
+  ADDITIONAL = '/additional'
+  CARD_PATH = 'gerar-cartao-grafica'
+  VIRTUAL_CARD_PATH = 'gerar-cartao-virtual'
+  BANK_SLIP_PATH = 'gerar-boleto-recarga'
+  P2P_PATH = 'p2ptransfer'
+  V1_PAYMENT = 'v1/payments'
+  PAYMENT = 'payments/'
+  PAYMENT_VALIDATE = 'payments/validate/'
+  VALIDATE = 'validate/'
+  P2P_ADJUSTMENT = 'p2ptransfer/adjustment/'
+  PAYMENT_ADJUSTMENT = 'payments/adjustment/'
+  BANK_SLIP = 'boletos'
+  CARD_PLAIN = 'cartoes'
+  CARD = 'cartoes/'
+  CARD_CONSULT = 'consultar-dados-reais'
+  PASSWORD_VALIDATION = 'validar-senha'
+  LIMIT_CONSULT = 'limites-disponibilidades'
+  CREATE_PF_CARD = 'gerar-cartao-pf'
+  CREATE_PJ_CARD = 'gerar-cartao-pj'
+  CREATE_NEW_CARD = 'gerar-nova-via'
+  UNLOCKED_INCORRECT_PASSWORD = 'desbloquear-senha-incorreta'
+  CREATE_DEBIT_CARD = 'gerar-Cartao-pre-pago'
+  REGISTER_PASSWORD = 'cadastrar-senha'
+  UNLOCKED = 'desbloquear'
+  ADD_HOLDER = 'atribuir-titular'
+  UPDATE_PASSWORD = 'alterar-senha'
+  REGISTER_PF_ACCOUNT = 'clientes/pf'
+  REGISTER_PJ_ACCOUNT = 'clientes/pj'
+  GENERATE_DOCUMENT = 'generatedocument'
+  BANK_TRANSFER = 'banktransfers'
+  BANK_TRANSFER_ADJUST = 'banktransfers/adjustment/'
+  TRANSPORT_CARDS = 'transportcards/card/'
+  TRANSPORT_CARDS_RECHARGE = 'transportcards/recharge'
+  TRANSPORT_CARDS_ACCOUNT = 'transportcards/account/'
+  RECHARGES = 'recharges'
+  DEALERS = 'dealers'
+  CONFIRM = 'confirm'
+  CONSULT = 'consult/'
+  ADJUSTMENT = 'adjustment/'
+  REGISTRATION = 'registration/'
+  REGISTRATION_WITH_TYPES = '?types=PRIVACY_POLICY&types=TERMS_OF_USE'
+  AGREEMENT = 'agreement'
+  USER = 'user'
+  LOGIN = 'login'
+  ASSIGN_PROFILE = 'assign-profile/'
+  UNASSIGN_PROFILE = 'unassign-profile/'
+  PRE_PAID = 'lotes-cartoes-pre-pagos'
+  BILLET = 'billet'
+  CARRIER_DETAILS = 'carrier-details'
+  CARRIER = 'carrier'
+  CALCULATE = 'calculate'
+  CEPS = 'ceps'
+  WAIVED = 'rate/waived'
+  KYC = 'knowyourclient/people'
+  PRODUCTS_PATH = 'produtos'
+  INDIVIDUALS = 'individuals'
+  REGISTRATIONS = 'registrations'
+  COMPANY_REGISTRATIONS = 'registered/companies'
+  COMPANY_NATIONAL_REGISTRATIONS = 'registrations/nationalRegistration'
+  INDIVIDUALS_ACCOUNTS = 'individuals/accounts'
+  SMS_PATH = 'omnichannel/sms'
+  RECEIPTS = 'receipts'
+  SCHEDULER = 'scheduler'
+  ALIAS_BANK = 'v1/accounts'
+  PAYMENT_SLIP_RECHARGE = 'recharge'
+  PAYMENT_SLIP_INVOICE = 'invoice'
+  ADD_NONAME_TO_ACCOUNT = 'atribuir-cartao-prepago'
+  DOCSPY_DOCUMENTS = 'docspy/documents'
+  DOCSPY_SELFIES = 'docspy/selfies'
+  DOCSPY_DOCUMENTS_RULES = 'docspy/documents/rules'
+  DOCSPY_SELFIES_RULES = 'docspy/selfies/rules'
+  DOCUMENTS = 'documents'
+  PENDING_DOCUMENTS = 'documents/status'
+  CONTROLS_LIMITS = '/controles-limites'
   CREATE_TEMPORARY_CARD = '/gerar-cartao-provisorio'
   CONTROLS_SETTINGS = 'controles-configuracoes'
   REPORTS = 'reports'
@@ -128,35 +128,35 @@ module CdtBaas
   CHANGE_CARD_PHASE = '/alterar-estagio'
   UPDATE_MAILING_ADDRESS = '/alterar-endereco-correspondencia'
 
-  #TED ONLINE
+  # TED ONLINE
 
-  PAY_CASHOUT = "v1/cashout"
-  CASHOUT_CANCEL = "v1/cashout/cancel-schedule"
-  TRANSACTIONS_CASHOUT = "v1/cashout/transactions"
-  RECEIPT_CASHOUT= "v1/cashout/receipt/"
+  PAY_CASHOUT = 'v1/cashout'
+  CASHOUT_CANCEL = 'v1/cashout/cancel-schedule'
+  TRANSACTIONS_CASHOUT = 'v1/cashout/transactions'
+  RECEIPT_CASHOUT = 'v1/cashout/receipt/'
 
-  #pix
-  CHECK_KEYS = "dict/v1/validate"
-  CREATE_KEY = "dict/v1"
-  CLAIM_KEY = "dict/v1/claim-key"
-  CLAIM_KEY_CONFIRMATION = "dict/v1/claim-key/confirmation"
+  # pix
+  CHECK_KEYS = 'dict/v1/validate'
+  CREATE_KEY = 'dict/v1'
+  CLAIM_KEY = 'dict/v1/claim-key'
+  CLAIM_KEY_CONFIRMATION = 'dict/v1/claim-key/confirmation'
 
-  STATIC_CODE = "code/v1/static-code"
-  DYNAMIC_CODE = "code/v1/dynamic-code"
-  DECODE_CODE = "code/v1/decode"
-  LIST_CODES = "code/v1"
+  STATIC_CODE = 'code/v1/static-code'
+  DYNAMIC_CODE = 'code/v1/dynamic-code'
+  DECODE_CODE = 'code/v1/decode'
+  LIST_CODES = 'code/v1'
 
-  PAY_PIX = "transfers/v1"
-  LIST_PIX = "transfers/v1/receipts"
-  NEW_LIST_PIX = "transfers/v1/receipts-list"
-  LIST_PIX_REVERSAL = "transfers/v1/reversal/receipts"
-  CANCEL_PIX = "transfers/v1/reversal"
+  PAY_PIX = 'transfers/v1'
+  LIST_PIX = 'transfers/v1/receipts'
+  NEW_LIST_PIX = 'transfers/v1/receipts-list'
+  LIST_PIX_REVERSAL = 'transfers/v1/reversal/receipts'
+  CANCEL_PIX = 'transfers/v1/reversal'
   PIX_RECEIPT = 'status'
   PIX_RECEIPT_REVERSAL = 'reversal'
 
   PIX_LIMITS = 'limits/v2'
 
-  PIX_SCHEDULER = "scheduler/v1/transfers"
+  PIX_SCHEDULER = 'scheduler/v1/transfers'
 
   DOWNLOAD = 'download'
   DOCSPY_DOCUMENTS_V1 = 'docspy/v1/documents'
--- lib/cdt_baas/modules/cdt_transfer.rb
+++ lib/cdt_baas/modules/cdt_transfer.rb
@@ -1,30 +1,28 @@
 module CdtBaas
-
   class CdtTransfer < CdtModule
-
     def initialize(token, env)
       startModule(token, env)
     end
 
     def bankTransfers(original_body)
       body =
-      {
-        "idOriginAccount": original_body[:idOriginAccount],
-        "identificator": Time.now.to_i.to_s,
-        "description": original_body[:description],
-        "beneficiary": {
-          "type": original_body[:beneficiary][:type],
-          "docIdCpfCnpjEinSSN": original_body[:beneficiary][:docIdCpfCnpjEinSSN],
-          "name": original_body[:beneficiary][:name],
-          "bankId": original_body[:beneficiary][:bankId],
-          "agency": original_body[:beneficiary][:agency],
-          "agencyDigit": "",
-          "account": original_body[:beneficiary][:account],
-          "accountDigit": original_body[:beneficiary][:accountDigit],
-          "accountType": original_body[:beneficiary][:accountType]
-        },
-        "value": original_body[:value]
-      }
+        {
+          "idOriginAccount": original_body[:idOriginAccount],
+          "identificator": Time.now.to_i.to_s,
+          "description": original_body[:description],
+          "beneficiary": {
+            "type": original_body[:beneficiary][:type],
+            "docIdCpfCnpjEinSSN": original_body[:beneficiary][:docIdCpfCnpjEinSSN],
+            "name": original_body[:beneficiary][:name],
+            "bankId": original_body[:beneficiary][:bankId],
+            "agency": original_body[:beneficiary][:agency],
+            "agencyDigit": '',
+            "account": original_body[:beneficiary][:account],
+            "accountDigit": original_body[:beneficiary][:accountDigit],
+            "accountType": original_body[:beneficiary][:accountType]
+          },
+          "value": original_body[:value]
+        }
       response = @request.post(@url + BANK_TRANSFER, body, true)
       transferResponse = CdtModel.new(response)
       generateResponse(transferResponse)
@@ -42,29 +40,30 @@ module CdtBaas
       generateResponse(transferResponse)
     end
 
-    #TED ONLINE
+    # TED ONLINE
 
     def bankTransfersCashout(original_body)
       body =
-      {
-        "accountId": original_body[:idOriginAccount],
-        "sourceBankNumber": original_body[:sourceBankNumber], 
-        "beneficiary": {
-          "type": original_body[:beneficiary][:type],
-          "name": original_body[:beneficiary][:name],
-          "nationalRegistration": original_body[:beneficiary][:docIdCpfCnpjEinSSN],
-          "bankNumber": original_body[:beneficiary][:bankId],
-          "bankBranchNumber": original_body[:beneficiary][:agency],
-          "bankBranchDigit": "",
-          "bankAccountNumber": original_body[:beneficiary][:account],
-          "bankAccountDigit": original_body[:beneficiary][:accountDigit],
-          "bankAccountType": original_body[:beneficiary][:accountType],
-          "accountId": original_body[:beneficiary][:idBeneficiaryAccount]
-        },
-        "amount": original_body[:value],
-        "description": original_body[:description],
-        "transferType": 3
-      }
+        {
+          "accountId": original_body[:idOriginAccount],
+          "sourceBankNumber": original_body[:sourceBankNumber],
+          "beneficiary": {
+            "type": original_body[:beneficiary][:type],
+            "name": original_body[:beneficiary][:name],
+            "nationalRegistration": original_body[:beneficiary][:docIdCpfCnpjEinSSN],
+            "bankNumber": original_body[:beneficiary][:bankId],
+            "bankBranchNumber": original_body[:beneficiary][:agency],
+            "bankBranchDigit": original_body[:beneficiary][:agency].last,
+            "bankAccountNumber": original_body[:beneficiary][:account],
+            "bankAccountDigit": original_body[:beneficiary][:accountDigit],
+            "bankAccountType": original_body[:beneficiary][:accountType].upcase,
+            "accountId": original_body[:beneficiary][:idBeneficiaryAccount]
+          },
+          "amount": original_body[:value],
+          "description": original_body[:description],
+          "transferType": 3
+        }
+      @url = @url.include?('hml') ? URL_CUSTOM_TRANSFER_HML : URL_CUSTOM_TRANSFER
       response = @request.post(@url + PAY_CASHOUT, body, true)
       transferResponse = CdtModel.new(response)
       generateResponse(transferResponse)
@@ -74,22 +73,24 @@ module CdtBaas
       body = {
         "transactionId": id_transaction
       }
+      @url = @url.include?('hml') ? URL_CUSTOM_TRANSFER_HML : URL_CUSTOM_TRANSFER
       response = @request.post(@url + CASHOUT_CANCEL)
       transferResponse = CdtModel.new(response, body, true)
       generateResponse(transferResponse)
     end
 
-    def getBankTransfersCashout()
+    def getBankTransfersCashout
+      @url = @url.include?('hml') ? URL_CUSTOM_TRANSFER_HML : URL_CUSTOM_TRANSFER
       response = @request.get(@url + TRANSACTIONS_CASHOUT)
       transferResponse = response
       generateResponse(transferResponse)
     end
 
     def getBankTransfersReceiptCashout(id)
+      @url = @url.include?('hml') ? URL_CUSTOM_TRANSFER_HML : URL_CUSTOM_TRANSFER
       response = @request.get(@url + RECEIPT_CASHOUT + id.to_s)
       transferResponse = response
       generateResponse(transferResponse)
     end
-
   end
 end
