aae88dab6d8c83c7a1b3fc963293ec185a2365d5
guilhermeandreuce <guilherme.monteiro@mobile2you.com.br>
2022-07-29 18:10:17 -0300

    refactor(BBC):(removing optional param from body)

--- lib/cdt_baas/modules/cdt_registration.rb
+++ lib/cdt_baas/modules/cdt_registration.rb
@@ -1,82 +1,75 @@
-
 module CdtBaas
+  class CdtRegistration < CdtModule
+    def initialize(token, env)
+      startModule(token, env)
+    end
 
-	class CdtRegistration < CdtModule
+    def createRegistration(body, version = 1)
+      @url = @url.gsub('api', 'companies') + 'v' + version.to_s + '/'
+      byebug
+      body[:company].delete(:tradeName) if body[:company][:tradename].blank?
+      response = @request.postWithHeader(@url + REGISTRATIONS, body, [jsonHeader])
+      CdtModel.new(response)
+    end
 
-        def initialize(token, env)
-            startModule(token, env)
-        end
+    def findRegistration(id, version = 1)
+      @url = @url.gsub('api', 'companies') + 'v' + version.to_s + '/'
+      response = @request.get(@url + REGISTRATIONS + '/' + id.to_s)
+      CdtModel.new(response)
+    end
 
-        def createRegistration(body, version = 1)
-            @url = @url.gsub("api", "companies") + 'v' + version.to_s + '/'
-            response = @request.postWithHeader(@url + REGISTRATIONS, body, [jsonHeader])
-            person = CdtModel.new(response)
-            person
-        end
+    def findCompany(id, version = 1)
+      @url = @url.gsub('api', 'companies') + 'v' + version.to_s + '/'
+      response = @request.get(@url + COMPANY_REGISTRATIONS + '/' + id.to_s)
+      CdtModel.new(response)
+    end
 
-        def findRegistration(id, version = 1)
-            @url = @url.gsub("api", "companies") + 'v' + version.to_s + '/'
-            response = @request.get(@url + REGISTRATIONS + '/' + id.to_s)
-            person = CdtModel.new(response)
-            person
-        end
-        
-        def findCompany(id, version = 1)
-            @url = @url.gsub("api", "companies") + 'v' + version.to_s + '/'
-            response = @request.get(@url + COMPANY_REGISTRATIONS + '/' + id.to_s)
-            person = CdtModel.new(response)
-            person
-        end
+    def recoverCompanyRegistration(cnpj, version = 1)
+      @url = @url.gsub('api', 'companies') + 'v' + version.to_s + '/'
+      response = @request.get(@url + COMPANY_NATIONAL_REGISTRATIONS + '/' + cnpj)
+      CdtModel.new(response)
+    end
 
-        def recoverCompanyRegistration(cnpj, version = 1)
-            @url = @url.gsub("api", "companies") + 'v' + version.to_s + '/'
-            response = @request.get(@url + COMPANY_NATIONAL_REGISTRATIONS + '/' + cnpj)
-            person = CdtModel.new(response)
-            person
-        end
-        def updateRegistration(id, body, version = 1)
-            @url = @url.gsub("api", "companies") + 'v' + version.to_s + '/'
-            response = @request.put(@url + COMPANY_REGISTRATIONS + '/' + id.to_s, body, [jsonHeader])
-            person = CdtModel.new(response)
-            person
-        end
+    def updateRegistration(id, body, version = 1)
+      @url = @url.gsub('api', 'companies') + 'v' + version.to_s + '/'
+      response = @request.put(@url + COMPANY_REGISTRATIONS + '/' + id.to_s, body, [jsonHeader])
+      CdtModel.new(response)
+    end
 
-        def updateRegistrationByIdRegistration(id, body, version = 1)
-            @url = @url.gsub("api", "companies") + 'v' + version.to_s + '/'
-            response = @request.put(@url + REGISTRATIONS + '/' + id.to_s, body, [jsonHeader])
-            person = CdtModel.new(response)
-            person
-        end
+    def updateRegistrationByIdRegistration(id, body, version = 1)
+      @url = @url.gsub('api', 'companies') + 'v' + version.to_s + '/'
+      response = @request.put(@url + REGISTRATIONS + '/' + id.to_s, body, [jsonHeader])
+      CdtModel.new(response)
+    end
 
+    def sendDocument(registration_id, params, options, version = 1)
+      @url = @url.gsub('api', 'companies') + 'v' + version.to_s + '/'
+      options[:headers] = {
+        'Content-Type' => 'image/jpeg'
+      }
 
-        def sendDocument(registration_id, params, options, version = 1)
-            @url = @url.gsub("api", "companies") + 'v' + version.to_s + '/'
-            options[:headers] = {
-                'Content-Type' => 'image/jpeg',
-            }
+      response = @request.postBinary(
+        @url + REGISTRATIONS + '/' + registration_id.to_s + '/' + DOCUMENTS + CdtHelper.conductorBodyToString(params), options
+      )
+      CdtModel.new(response)
+    end
 
-            response = @request.postBinary(@url + REGISTRATIONS + '/' + registration_id.to_s + "/" + DOCUMENTS + CdtHelper.conductorBodyToString(params), options)
-            person = CdtModel.new(response)
-            person
-        end
+    def sendDocumentPdf(registration_id, params, options, version = 1)
+      @url = @url.gsub('api', 'companies') + 'v' + version.to_s + '/'
+      options[:headers] = {
+        'Content-Type' => 'application/pdf'
+      }
 
-        def sendDocumentPdf(registration_id, params, options, version = 1)
-          @url = @url.gsub("api", "companies") + 'v' + version.to_s + '/'
-          options[:headers] = {
-              'Content-Type' => 'application/pdf',
-          }
+      response = @request.postBinary(
+        @url + REGISTRATIONS + '/' + registration_id.to_s + '/' + DOCUMENTS + CdtHelper.conductorBodyToString(params), options
+      )
+      CdtModel.new(response)
+    end
 
-          response = @request.postBinary(@url + REGISTRATIONS + '/' + registration_id.to_s + "/" + DOCUMENTS + CdtHelper.conductorBodyToString(params), options)
-          person = CdtModel.new(response)
-          person
-      end
-
-       def getDocuments(registration_id, version = 1)
-            @url = @url.gsub("api", "companies") + 'v' + version.to_s + '/'
-            response = @request.get(@url + REGISTRATIONS + '/' + registration_id.to_s + "/" + DOCUMENTS)
-            person = CdtModel.new(response)
-            person
-        end
-	end
-
+    def getDocuments(registration_id, version = 1)
+      @url = @url.gsub('api', 'companies') + 'v' + version.to_s + '/'
+      response = @request.get(@url + REGISTRATIONS + '/' + registration_id.to_s + '/' + DOCUMENTS)
+      CdtModel.new(response)
+    end
+  end
 end
