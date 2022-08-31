298a6f8cbd3e8976ba8c9f33f779d932019a5bbd
andre-m2y <jose.moreira@mobile2you.com.br>
2022-08-31 09:49:15 -0300

    created trusted account and rubocop

--- lib/cdt_baas/modules/cdt_transactions_limits.rb
+++ lib/cdt_baas/modules/cdt_transactions_limits.rb
@@ -1,15 +1,13 @@
 require 'date'
 
 module CdtBaas
-
   class CdtTransactionsLimits < CdtModule
-
-    def initialize(token, env) 
+    def initialize(token, env)
       startModule(token, env)
     end
 
     def limits_url
-      @url.gsub("api", "limits")
+      @url.gsub('api', 'limits')
     end
 
     def list_limits(account, group)
@@ -18,25 +16,25 @@ module CdtBaas
       response = @request.get(url, [jsonHeader])
       generateResponse(response)
     end
-    
+
     def create_limit(body, account, group)
       url = "#{limits_url}#{LIMITS}/accounts/#{account}/services-groups/#{group.to_i}"
       response = @request.post(url, body, true)
       generateResponse(response)
     end
-    
+
     def list_account_timetables(account)
       url = "#{limits_url}#{LIMITS}/accounts/#{account}/timetables"
       response = @request.get(url, [jsonHeader])
       generateResponse(response)
     end
-    
+
     def update_account_timetables(body, account)
       url = "#{limits_url}#{LIMITS}/accounts/#{account}/timetables"
       response = @request.put(url, body)
       generateResponse(response)
     end
-    
+
     def list_limits_all_groups(body)
       url = limits_url + LIMITS
       response = @request.get(url + CdtHelper.conductorBodyToString(body), [jsonHeader])
@@ -48,6 +46,24 @@ module CdtBaas
       url += CdtHelper.conductorBodyToString({ idServicesGroup: group.to_i })
       response = @request.get(url, [jsonHeader])
       generateResponse(response)
+    end
+
+    def create_batch_limit_for_trusted_destination(body, account, _group)
+      request = []
+      pending = []
+      g = 1
+      5.times do
+        url = "#{limits_url}#{LIMITS}/accounts/#{account}/trusted-destinations"
+        body[:idServicesGroup] = g
+        result = @request.post(url, body, true)
+        if result[:statusCode] > 201
+          pending.push(result)
+        else
+          request.push(result)
+        end
+        g += 1
+      end
+      request.present? ? request : pending
     end
   end
 end
