a639cd41e8d6bcabb97d2adf2f59e0cf4b1e7031
caio lopes <caio.lopes@mobile2you.com.br>
2022-09-08 14:53:11 -0300

    add route

--- lib/cdt_baas/modules/cdt_individual.rb
+++ lib/cdt_baas/modules/cdt_individual.rb
@@ -1,73 +1,81 @@
 
 module CdtBaas
 
-	class CdtIndividual < CdtModule
+  class CdtIndividual < CdtModule
 
-        def initialize(token, env)
-            startModule(token, env)
-        end
+    def initialize(token, env)
+      startModule(token, env)
+    end
 
-        def createAccount(body, version = 2)
-            response = @request.postWithHeader(@url + "v#{version}/" + INDIVIDUALS_ACCOUNTS, body, [jsonHeader])
-            person = CdtModel.new(response)
-            person
-        end
+    def createAccount(body, version = 2)
+      response = @request.postWithHeader(@url + "v#{version}/" + INDIVIDUALS_ACCOUNTS, body, [jsonHeader])
+      person = CdtModel.new(response)
+      person
+    end
 
-        def createPerson(body, version = 2)
-            response = @request.postWithHeader(@url + "v#{version}/" + INDIVIDUALS, body, [jsonHeader])
-            person = CdtModel.new(response)
-            person
-        end
+    def createPerson(body, version = 2)
+      response = @request.postWithHeader(@url + "v#{version}/" + INDIVIDUALS, body, [jsonHeader])
+      person = CdtModel.new(response)
+      person
+    end
 
-        def addAdditionalPerson(id, body, version = 2)
-            response = @request.postWithHeader(@url + "v#{version}/" + INDIVIDUALS_ACCOUNTS + '/' + id.to_s + ADDITIONAL, body, [jsonHeader])
-            person = CdtModel.new(response)
-            person
-        end
+    def addAdditionalPerson(id, body, version = 2)
+      response = @request.postWithHeader(@url + "v#{version}/" + INDIVIDUALS_ACCOUNTS + '/' + id.to_s + ADDITIONAL, body, [jsonHeader])
+      person = CdtModel.new(response)
+      person
+    end
 
-        def findPerson(id, version = 2, params: {})
-            response = @request.get(@url + "v#{version}/" + INDIVIDUALS + '/' + id.to_s + CdtHelper.conductorBodyToString(params))
-            person = CdtModel.new(response)
-            person
-        end
+    def findPerson(id, version = 2, params: {})
+      response = @request.get(@url + "v#{version}/" + INDIVIDUALS + '/' + id.to_s + CdtHelper.conductorBodyToString(params))
+      person = CdtModel.new(response)
+      person
+    end
 
-        def findPersonSPD(id, version = 2)
-            response = @request.get(@url + "v#{version}/" + INDIVIDUALS + '/' + id.to_s + "?statusSPD=true")
-            person = CdtModel.new(response)
-            person
-        end
+    def findPersonSPD(id, version = 2)
+      response = @request.get(@url + "v#{version}/" + INDIVIDUALS + '/' + id.to_s + "?statusSPD=true")
+      person = CdtModel.new(response)
+      person
+    end
 
-        def getPersons(body, version = 2)
-            response = @request.get(@url + "v#{version}/" + INDIVIDUALS + CdtHelper.conductorBodyToString(body))
-            person = CdtModel.new(response)
-            person
-        end
+    def getPersons(body, version = 2)
+      response = @request.get(@url + "v#{version}/" + INDIVIDUALS + CdtHelper.conductorBodyToString(body))
+      person = CdtModel.new(response)
+      person
+    end
 
-        def updatePerson(id, body, version = 2)
-            response = @request.put(@url + "v#{version}/" + INDIVIDUALS + '/' + id.to_s , body, [jsonHeader])
-            person = CdtModel.new(response)
-            person
-        end
+    def updatePerson(id, body, version = 2)
+      response = @request.put(@url + "v#{version}/" + INDIVIDUALS + '/' + id.to_s , body, [jsonHeader])
+      person = CdtModel.new(response)
+      person
+    end
 
-        def getDocuments(registration_id, version = 2)
-            response = @request.get(@url + "v#{version}/" + INDIVIDUALS + '/' + registration_id.to_s + "/" + DOCUMENTS)
-            person = CdtModel.new(response)
-            person
-        end
-        def getPendingDocuments(registration_id, version = 2)
-            response = @request.get(@url + "v#{version}/" + INDIVIDUALS + '/' + registration_id.to_s + "/" + PENDING_DOCUMENTS)
-            person = CdtModel.new(response)
-            person
-        end
+    def getDocuments(registration_id, version = 2)
+      response = @request.get(@url + "v#{version}/" + INDIVIDUALS + '/' + registration_id.to_s + "/" + DOCUMENTS)
+      person = CdtModel.new(response)
+      person
+    end
+    def getPendingDocuments(registration_id, version = 2)
+      response = @request.get(@url + "v#{version}/" + INDIVIDUALS + '/' + registration_id.to_s + "/" + PENDING_DOCUMENTS)
+      person = CdtModel.new(response)
+      person
+    end
 
-        def sendDocument(registration_id, params, options, version = 2)
-            options[:headers] = {'Content-Type' => 'image/jpeg'} if options[:headers].blank?
+    def sendDocument(registration_id, params, options, version = 2)
+      options[:headers] = {'Content-Type' => 'image/jpeg'} if options[:headers].blank?
 
-            response = @request.postBinary(@url + "v#{version}/" + INDIVIDUALS + '/' + registration_id.to_s + "/" + DOCUMENTS + CdtHelper.conductorBodyToString(params), options)
-            person = CdtModel.new(response)
-            person
-        end
+      response = @request.postBinary(@url + "v#{version}/" + INDIVIDUALS + '/' + registration_id.to_s + "/" + DOCUMENTS + CdtHelper.conductorBodyToString(params), options)
+      person = CdtModel.new(response)
+      person
+    end
 
-	end
+    def sendDocumentPNG(registration_id, params, options, version = 2)
+      options[:headers] = {'Content-Type' => 'image/png'} if options[:headers].blank?
+
+      response = @request.postBinary(@url + "v#{version}/" + INDIVIDUALS + '/' + registration_id.to_s + "/" + DOCUMENTS + CdtHelper.conductorBodyToString(params), options)
+      person = CdtModel.new(response)
+      person
+    end
+
+  end
 
 end
