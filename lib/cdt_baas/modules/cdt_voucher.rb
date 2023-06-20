d93f50e6c5201596995969fa26ed901387748d77
caio lopes <caio.lopes@mobile2you.com.br>
2023-06-20 17:03:36 -0300

    voucher

--- lib/cdt_baas/modules/cdt_voucher.rb
+++ lib/cdt_baas/modules/cdt_voucher.rb
@@ -1,38 +1,34 @@
 module CdtBaas
 
-	class CdtVoucher < CdtModule
+  class CdtVoucher < CdtModule
 
-		def initialize(token, env)
-			startModule(token, env)
-		end
+    def initialize(token, env)
+      startModule(token, env)
+    end
 
-		def getMerchants(version = 1)
-			url = @url.gsub("api", "voucher") + 'v' + version.to_s
-			response = @request.get(url + MERCHANTS)
-	        cdt_model = CdtModel.new(response)
-	        cdt_model
-		end
+    def getMerchants(version = 1)
+      url = @url.gsub("api", "voucher") + 'v' + version.to_s
+      response = @request.get(url + MERCHANTS)
+      response
+    end
 
-		def createOrder(merchantId, version = 1)
-			url = @url.gsub("api", "voucher") + 'v' + version.to_s + '/'
-			response = @request.post(@url + merchantId + CREATE_ORDER, {}, true)
-	        cdt_model = CdtModel.new(response)
-	        cdt_model
-		end
+    def createOrder(merchantId, version = 1)
+      url = @url.gsub("api", "voucher") + 'v' + version.to_s + '/'
+      response = @request.post(@url + merchantId + CREATE_ORDER, {}, true)
+      response
+    end
 
-		def confirmOrder(orderId, body, version = 1)
-			url = @url.gsub("api", "voucher") + 'v' + version.to_s + '/'
-			response = @request.post(@url + orderId  + CONFIRM_ORDER, body, true)
-	        cdt_model = CdtModel.new(response)
-	        cdt_model
-		end
+    def confirmOrder(orderId, body, version = 1)
+      url = @url.gsub("api", "voucher") + 'v' + version.to_s + '/'
+      response = @request.post(@url + orderId  + CONFIRM_ORDER, body, true)
+      response
+    end
 
-	 	def getReceipts(body, version = 1)
-	        url = @url.gsub("api", "voucher") + 'v' + version.to_s + '/'
-	        response = @request.get(url + RECEIPTS + CdtHelper.conductorBodyToString(body))
-	        cdt_model = CdtModel.new(response)
-	        cdt_model
-	    end
+    def getReceipts(body, version = 1)
+      url = @url.gsub("api", "voucher") + 'v' + version.to_s + '/'
+      response = @request.get(url + RECEIPTS + CdtHelper.conductorBodyToString(body))
+      response
+    end
 
-	end
+  end
 end
