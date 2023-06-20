2c2799b4edd776e1e19eab5efd7b95fb48a6d80e
caio lopes <caio.lopes@mobile2you.com.br>
2023-06-20 17:38:25 -0300

    voucher

--- lib/cdt_baas/modules/cdt_voucher.rb
+++ lib/cdt_baas/modules/cdt_voucher.rb
@@ -14,13 +14,13 @@ module CdtBaas
 
     def createOrder(merchantId, version = 1)
       url = @url.gsub("api", "voucher") + 'v' + version.to_s + '/'
-      response = @request.post(@url + merchantId + CREATE_ORDER, {}, true)
+      response = @request.post(url + merchantId + CREATE_ORDER, {}, true)
       response
     end
 
     def confirmOrder(orderId, body, version = 1)
       url = @url.gsub("api", "voucher") + 'v' + version.to_s + '/'
-      response = @request.post(@url + orderId  + CONFIRM_ORDER, body, true)
+      response = @request.post(url + orderId  + CONFIRM_ORDER, body, true)
       response
     end
 
